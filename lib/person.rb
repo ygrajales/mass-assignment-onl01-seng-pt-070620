9504282efab671983a4aa146f519fb546ee0b9ef
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-10 16:43:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   
-  person = {name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t
+  person = {name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, 
   
   def initialize(person)
   
