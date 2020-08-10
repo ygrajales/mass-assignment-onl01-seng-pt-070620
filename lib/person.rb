1ca05d061f6a3f7f8a3133cf891bd14a91b89766
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-10 16:51:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   person_attributes = {name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:}
   
   def initialize(person_attributes)
-    person_attributes.each{|key, value| self.send
+    person_attributes.each{|key, value| self.send(())
   end
   
   
